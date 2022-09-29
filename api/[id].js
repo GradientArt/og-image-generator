a5bccf67229bb993fa06b31ac3f499b71c10a235
Yoav Kadosh <yoavkad@microsoft.com>
2022-09-30 02:12:47 +0300

    Updated CORS headers

--- api/[id].js
+++ api/[id].js
@@ -1,16 +1,8 @@
 import chromium from 'chrome-aws-lambda';
 import playwright from 'playwright-core';
 
-const ALLOWED_ORIGINS = [
-    'https://dient.art',
-    'https://gra.dient.art',
-];
-
 export default async (req, res) => {
     try {
-        if (!ALLOWED_ORIGINS.includes(req.headers.host)) {
-            throw 'Invalid origin';
-        }
         const executablePath = process.env.NODE_ENV === 'development' ? undefined : await chromium.executablePath;
         const browser = await playwright.chromium.launch({
             args: chromium.args,
@@ -25,7 +17,7 @@ export default async (req, res) => {
         await browser.close();
         res.setHeader('Content-Type', 'image/png');
         res.setHeader('Cache-Control', 's-maxage=31536000, max-age=31536000, stale-while-revalidate');
-        res.setHeader('Access-Control-Allow-Origin', req.headers.host);
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.status(200);
         res.end(image, 'binary');
     } catch(e) {
